7215bc06a8a47cd4166acf077efd315dd3f90194
fangkyi03 <fangkyi02@live.cn>
2019-11-07 01:39:14 +0800

    1.修改提交

--- test.js
+++ test.js
@@ -9,8 +9,9 @@ imgNode.init({
         'web': path.resolve('./'),
         'taro': path.resolve('./'),
     },
-    'ctoken':'tMdXXonr-k2X0QYcN8tHrBqpmb_wozHp3JhU',
-    'cookie':'ctoken=UuB2kW2446pzqpuHpMjycH4L; EGG_SESS_DAVINCI=B8G5XPvkjePB-ITpJ2Se0cJV9OMYPrHlX5Fuhegay2nQ2V-hLqunk-sSfyky3kNyJxevM4z2HNctA0IwjU6C5u7gv08mbfhEgTLHOGThnDQ=; cna=v+BJFiJ16jkCASe8LnIZR/Tq; isg=BPb2H_U1t4NOBENnYo_4ALhHRyr4FzpRsz6QR2DfsVl0o5Q9yaOuY84VvzdqCzJp'
+    // 请在这里输入自己的imgcook token与cookie
+    'ctoken':'',
+    'cookie':''
 })
 co(function *() {
     yield imgNode.scan()
